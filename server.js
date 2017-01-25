9c664d1e7d33b913a0844fef515ef28dfc0d550a
Astrid Wang <Astidw@g.ucla.edu>
2017-01-25 04:22:24 -0800

    ugh

--- server.js
+++ server.js
@@ -1,24 +1,34 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const bruinMessenger = require('./app')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
 
 // set the view engine to handlebars
 app.use(express.static('public'))
 app.set('view engine', 'hbs')
 
-app.get('/', (req, res, next) => {
-  res.render('login')
-})
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
 
-app.get('/rooms', (req, res, next) => {
-  res.render('rooms')
-})
+// logging
+app.use(morgan('combined'))
+
+// include session middleware
+app.use(bruinMessenger.session)
+
+// Register our bruinMessenger routes
+app.use('/', bruinMessenger.router)
 
 // 404 for all unregistered routes
 app.use((req, res, next) => {
   res.status(404).render('404')
 })
 
-app.listen(port, () => {
-  console.log('BruinMessenger running on port: ', port)
-})
+/**
+ * Set up the ioServer and listen on port `port`
+ */
+bruinMessenger.ioServer(app).listen(port, () => {
+	console.log("Started BruinMessenger on port", port);
+});
